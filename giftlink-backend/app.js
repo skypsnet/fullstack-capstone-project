a21be4595bb96bae8dfb189f41945f342c6718a2
Ricardo Balam <rbalamcm@gmail.com>
2025-02-04 17:15:51 -0600

    Authentication FrontEnd

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -5,8 +5,6 @@ const cors = require('cors');
 const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
-
 
 const app = express();
 app.use("*",cors());
