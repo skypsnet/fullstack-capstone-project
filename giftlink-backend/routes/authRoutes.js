af970363d3a8ace495ae54ba41569c29f3658026
Ricardo Balam <rbalamcm@gmail.com>
2025-02-04 18:38:19 -0600

    Login Page Back and Front connected

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -42,4 +42,45 @@ router.post('/register', async (req, res)=>{
           
 });
 
+router.post('/login', async (req, res)=>{
+    
+    try{
+    const db = await connectToDatabase();
+    const collection = db.collection('users');
+    const {email, password} =  req.body
+    console.log(email, password)
+    const theUser = await collection.findOne({email: email});
+
+    if(theUser){
+       let result = await bcryptjs.compare(password, theUser.password);
+       if(!result){
+          logger.error("Passwords do not match")
+          return res.status(404).json({error: "Wrong password!"});
+       }
+       
+       const userName = theUser.firstName;
+       const userEmail = theUser.email;
+
+       const payload = {
+          user: {
+            id: theUser._id.toString(),
+          }
+       }
+
+       const authtoken = jwt.sign(payload, JWT_SECRET);
+       logger.info('User logged in successfully')
+       return res.status(200).json({authtoken, userName, userEmail})
+
+
+    }else{
+        logger.error('User not found')
+        return res.status(404).json({message: "user not found"})
+    }
+
+    }catch(e){
+        console.error(e.message);
+    }
+
+})
+
 module.exports = router;
